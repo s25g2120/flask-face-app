ab430cf5ad2bac455b9adf0ae7132d77dac4d9cc
25G2143 <s25G2143PC@s.chibakoudai.jp>
2025-11-25 06:36:06 +0000

    先週の終わり

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request and
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_migrate import Migrate
 from models import Task, User, db
